b2a6cc5187094c9e44ef84cd13faf1d282c77d46
DCRobledo <daniel.canorobledo@gmail.com>
2020-06-17 18:02:47 +0200

    Added essential Unity files + gitignore

---
+++ Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour
+{
+    public GameObject roadTile;
+    public int roadLength = 30;
+    public int roadWidth = 5;
+
+    public GameObject cameraGO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraGO.transform.position = new Vector3(roadWidth/2f, 0, -10);
+
+        for (int i = 0; i<roadLength; i++){
+            for(int j = 0; j<roadWidth; j++){
+                GameObject insTile = GameObject.Instantiate(roadTile, this.transform);
+                insTile.transform.position = new Vector3(j, i, 0);
+                insTile.transform.name = "RoadTile("+ j +","+ i +")";
+                int var = 0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
