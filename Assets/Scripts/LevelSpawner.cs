1032eafccb1df214455d2be8ab997e561d81ef68
DCRobledo <daniel.canorobledo@gmail.com>
2020-06-19 17:58:11 +0200

    Walls & Rocks Collisions Implemented + Camera Fixing + Polish Level Generation - No Bugs - No Refactor Needed

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -4,50 +4,44 @@ using UnityEngine;
 
 public class LevelSpawner : MonoBehaviour
 {
-    public int roadLength = 30;
-    public int roadWidth = 5;
+    public int roadWidth = 50;
+    public int previousYLevel = -1;
+
+    public List<GameObject> roadCollidables = new List<GameObject>();
 
     public GameObject cameraGO;
     public GameObject roadTile;
     public GameObject carObj;
 
-
     public Sprite leftRoad;
     public Sprite rightRoad;
-
     public Sprite leftStartRoad;
     public Sprite middleStartRoad;
     public Sprite rightStartRoad;
-
+    public Sprite plainRoad;
     public Sprite grassTile;
 
     public float maxYLevel = -1f;
-    public int previousYLevel = -1;
+    
 
-    void Start()
-    {
+    void Start() {
         cameraGO.transform.position = new Vector3((roadWidth-1) / 2f, .5f, -10);
-
         SpawnStartRoad();
     }
 
-    private void SpawnStartRoad()
-    {
+    private void SpawnStartRoad() {
         for (int x = 0; x < roadWidth; x++)
         {
             GameObject insTile = GameObject.Instantiate(roadTile, this.transform);
-            insTile.transform.position = new Vector3(x, -1, 0);
-            insTile.transform.name = "RoadTile(" + x + "," + -1 + ")";
+            insTile.transform.position = new Vector3(x, -1, -5);
+            insTile.transform.name = "StartRoadTile(" + x + "," + -1 + ")";
             insTile.GetComponent<SpriteRenderer>().sprite = middleStartRoad;
 
             if (x == 0)
                 insTile.GetComponent<SpriteRenderer>().sprite = leftStartRoad;
             if (x == roadWidth - 1)
                 insTile.GetComponent<SpriteRenderer>().sprite = rightStartRoad;
-            SpawnStartGrass(x);
-        }
-       
-
+        } 
     }
 
     private void SpawnStartGrass(int x)
@@ -59,8 +53,9 @@ public class LevelSpawner : MonoBehaviour
     }
 
     private void SpawnRoad(float y){
-        for (int x = 0; x < roadWidth; x++) {
-            GameObject insTile = GameObject.Instantiate(roadTile, this.transform);
+        for (int x = -2; x < roadWidth+2; x++)
+        {
+            GameObject insTile = GameObject.Instantiate(getGOToSpawn(x), this.transform);
             insTile.transform.position = new Vector3(x, y, 0);
             insTile.transform.name = "RoadTile(" + x + "," + y + ")";
             insTile.AddComponent<DestroyMe>().carObj = carObj;
@@ -68,31 +63,50 @@ public class LevelSpawner : MonoBehaviour
                 insTile.GetComponent<SpriteRenderer>().sprite = leftRoad;
             if (x == roadWidth - 1)
                 insTile.GetComponent<SpriteRenderer>().sprite = rightRoad;
+
+            SpawnRocks(y, x, insTile);
         }
     }
 
-    private void SpawnGrass(float y){
-        for(int x=-2; x<roadWidth+2; x++){
-            GameObject insTile = GameObject.Instantiate(roadTile, this.transform);
-            insTile.transform.position = new Vector3(x, y, 2);
-            insTile.transform.name = "RoadTile(" + x + "," + -1 + ")";
-            insTile.GetComponent<SpriteRenderer>().sprite = grassTile;
+    private void SpawnRocks(float y, int x, GameObject insTile)
+    {
+        int rnd = Random.Range(0, 100);
+        if (rnd <= 1 && x >= 0 && x <= roadWidth - 1)
+        {
+            int listRnd = Random.Range(0, roadCollidables.Count - 1);
+            GameObject collidableObj = GameObject.Instantiate(roadCollidables[listRnd]);
+            collidableObj.transform.position = new Vector3(x, y, -5);
+            insTile.transform.name = "Rock(" + x + "," + y + ")";
             insTile.AddComponent<DestroyMe>().carObj = carObj;
         }
     }
 
-    void Update()
+    private GameObject getGOToSpawn(int x){
+        GameObject go = roadTile;
+        go.GetComponent<SpriteRenderer>().sprite = plainRoad;
+        SelectSprite(x, go);
+
+        return go;
+    }
+
+    private void SelectSprite(int x, GameObject go)
     {
+        if (x == -2 || x == -1 || x == roadWidth + 1 || x == roadWidth)
+            go.GetComponent<SpriteRenderer>().sprite = grassTile;
+        else if (x == roadWidth - 1)
+            go.GetComponent<SpriteRenderer>().sprite = rightRoad;
+        else if (x == 0)
+            go.GetComponent<SpriteRenderer>().sprite = leftRoad;
+    }
+
+    void Update() {
         SpawnLevel();
     }
 
-    private void SpawnLevel()
-    {
-        if (carObj.transform.position.y > maxYLevel-6)
-        {
+    private void SpawnLevel() {
+        if (carObj.transform.position.y > maxYLevel-10){
             previousYLevel = previousYLevel + 1;
             SpawnRoad(previousYLevel);
-            SpawnGrass(previousYLevel);
             maxYLevel = previousYLevel;
         }
     }
